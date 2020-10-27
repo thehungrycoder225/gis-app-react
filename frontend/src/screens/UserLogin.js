28a3475b2ea0a6d4da21b93a37fb95fcb7398124
thehungrycoder225 <cyber_sky09@yahoo.com>
2020-10-27 23:01:47 +0800

    Added Login and Login Reducers

--- frontend/src/screens/UserLogin.js
+++ frontend/src/screens/UserLogin.js
@@ -1,30 +1,77 @@
-import React from 'react';
-import { Container, Button, Form, Card } from 'react-bootstrap';
-const UserLogin = () => {
-  return (
-    <>
-      <Container>
-        <Card>
-          <Card.Header>User Login</Card.Header>
-          <Card.Body>
-            <Form>
-              <Form.Group controlId='formBasicEmail'>
-                <Form.Label>Username</Form.Label>
-                <Form.Control type='text' placeholder='Enter username' />
-              </Form.Group>
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, Form, Card, Row, Col } from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { login } from '../actions/userActions';
+const UserLogin = ({ location, history }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
 
-              <Form.Group controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Password' />
-              </Form.Group>
-              <Button variant='primary' type='submit'>
-                Submit
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Container>
-    </>
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(username, password));
+  };
+  return (
+    <FormContainer>
+      <Card>
+        <Card.Header>
+          {' '}
+          <h1>User Login</h1>{' '}
+          {error && <Message variant='danger'>{error}</Message>}
+          {loading && <Loader />}
+        </Card.Header>
+        <Card.Body>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='username'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant='success' type='submit'>
+              Log In
+            </Button>
+          </Form>
+          <Row className='py-3'>
+            <Col>
+              New User?{' '}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : '/register'}
+              >
+                {' '}
+                Register{' '}
+              </Link>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </FormContainer>
   );
 };
 
