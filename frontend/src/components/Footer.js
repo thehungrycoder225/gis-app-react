a34ba8b528a3b85c290c47ea44f3f22cb1c15309
thehungrycoder225 <cyber_sky09@yahoo.com>
2020-11-09 11:52:42 +0800

    Added Sics Dev Logo

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import logo from '../extras/sicslogo.svg';
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
           <Col className='text-center py-3 mt-5 mb-0'>
-            Copyright &copy;SiCS Dev: We Build Things 2020
+            MARCGeo &copy;
+            <img src={logo} alt='Sics Dev Team Logo' /> : We Build Things 2020
           </Col>
         </Row>
       </Container>
